65398f29c197032519d4d01a6b22d47ff073d745
Christian Mack <sfcamack@icloud.com>
2023-11-16 20:11:56 -0500

    scene setup. model loaded.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,32 @@
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { Loader } from "../components";
+
+import Island from "../models/Island";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full h-screen relative">
+      {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        POPUP
+      </div> */}
+
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <ambientLight />
+          <pointLight />
+          <directionalLight />
+          <spotLight />
+          <hemisphereLight />
+
+          <Island />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
 };
 
 export default Home;
