05ff921e897f671a4ed35e20495fa8864f4f5572
Christian Mack <sfcamack@icloud.com>
2023-11-16 19:22:12 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Navbar } from "./components";
+import { About, Home, Projects, Contact } from "./pages";
+
+export default function App() {
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </main>
+  );
+}
