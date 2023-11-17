8bec9043b864d7ee469c56fe1ba6516489d3751e
Christian Mack <sfcamack@icloud.com>
2023-11-16 20:27:50 -0500

    model lighting added. sky component started.

---
+++ src/models/Sky.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sky = () => {
+  return <div>Sky</div>;
+};
+
+export default Sky;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,22 @@ import { Loader } from "../components";
 import Island from "../models/Island";
 
 const Home = () => {
+  const adjustModelForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustModelForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -16,13 +32,30 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <ambientLight />
-          <pointLight />
-          <directionalLight />
-          <spotLight />
-          <hemisphereLight />
+          {/* simulates light coming from distant source, i.e. sunlight */}
+          <directionalLight position={[1, 1, 1]} intensity={2} />
 
-          <Island />
+          {/* illuminates all objects in the scene equally without casting shadows */}
+          <ambientLight intensity={0.5} />
+
+          {/* emits light in all directions from a single point. Unneeded for outdoor scenes */}
+          {/* <pointLight /> */}
+
+          {/* it emits light in one direction in the shape of a cone */}
+          {/* <spotLight /> */}
+
+          {/* illuminates the scene with a gradient */}
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
         </Suspense>
       </Canvas>
     </section>
